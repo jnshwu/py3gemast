5dadefc76c317d34be4dd0d1416c344b5d01b30c
jinshiwu <jnshwu@gmail.com>
2017-02-06 00:33:17 -0600

    change a bit code

--- jinshi_code/tuple_example.py
+++ jinshi_code/tuple_example.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 #Created by jinshiwu on 2/6/17.
 
+tuple='hello','world'
+tuple1='hello',
+tuple3=('hello', 'world', 'jinshi')
+
+print(tuple[0])
+print(tuple1)
+print(tuple3)
